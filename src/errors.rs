bb9fc28c810f1e14797b585e103b3c21424ba323
neezar <rforlyffe@gmail.com>
2024-02-09 21:58:14 +0800

    update error

--- src/errors.rs
+++ src/errors.rs
@@ -2,8 +2,8 @@ use thiserror::Error;
 
 #[derive(Error, Debug)]
 pub enum ErrorResponse {
-    #[error("invalid_request")]
-    InvalidRequest,
-    #[error("no_todo")]
-    NoTodo,
+    #[error("invalid_request, error={error}")]
+    InvalidRequest { error: String },
+    #[error("no_todo with id={id}")]
+    NoTodo { id: String },
 }
